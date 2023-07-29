46c8960ff0ebf99a55cc15ce719683b5b3e06397
Billy Farrell <billyfarrell95@gmail.com>
2023-07-29 15:03:22 -0500

    Start button renders questions and options

---
+++ js/app.js
@@ -0,0 +1,27 @@
+import triviaData from "./data.js"
+
+const startBtn = document.getElementById("start");
+
+startBtn.addEventListener("click", startTrivia);
+
+function startTrivia() {
+    renderUI();
+    startBtn.classList.toggle("hide-btn");
+}
+
+function renderUI() {
+    const triviaWrapper = document.getElementById("trivia-wrapper");
+    for (let i = 0; i < triviaData.length; i++) {
+        let newH2 = document.createElement("h2");
+        newH2.innerText = triviaData[i].question;
+        let newQuestionWrapper = document.createElement("div");
+        newQuestionWrapper.classList.add("question-wrapper");
+        triviaWrapper.append(newQuestionWrapper);
+        newQuestionWrapper.append(newH2);
+        for (let i = 0; i < triviaData[i].options.length; i++) {
+            let newBtn = document.createElement("button");
+            newBtn.innerText = triviaData[i].options[i];
+            newQuestionWrapper.append(newBtn);
+        }
+    };
+};
---
+++ js/data.js
@@ -0,0 +1,55 @@
+// Trivia questions and answers
+const triviaData = [
+    {
+        question: "Which of the following is a programming language commonly used for web development?",
+        options: ["HTML", "CSS", "JavaScript", "All of the above"],
+        answer: "All of the above"
+    },
+    {
+        question: "What does HTML stand for?",
+        options: ["Hyperlinks and Text Markup Language", "Hypertext Markup Language", "Home Tool Markup Language", "Hyperlink and Text Markup Leveler"],
+        answer: "Hypertext Markup Language"
+    },
+    {
+        question: "Which programming language is primarily used for adding interactivity and dynamic content to websites?",
+        options: ["Python", "Java", "JavaScript", "Ruby"],
+        answer: "JavaScript"
+    },
+    {
+        question: "Cascading Style Sheets (CSS) is mainly used for what purpose in web development?",
+        options: ["Adding interactivity", "Handling server-side operations", "Structuring web pages", "Styling and layout"],
+        answer: "Styling and layout"
+    },
+    {
+        question: "Which of the following is NOT a web browser?",
+        options: ["Chrome", "Firefox", "Safari", "MySQL"],
+        answer: "MySQL"
+    },
+    {
+        question: "What is the primary purpose of the DOCTYPE declaration in an HTML document?",
+        options: ["It specifies the version of HTML used in the document.", "It links the document to an external stylesheet.", "It declares the character encoding of the document.", "It informs the browser which rendering mode to use."],
+        answer: "It informs the browser which rendering mode to use."
+    },
+    {
+        question: "Which tag is used to link an external JavaScript file to an HTML document?",
+        options: ["<script>", "<link>", "<js>", "<javascript>"],
+        answer: "<script>"
+    },
+    {
+        question: "What does the acronym API stand for in web development?",
+        options: ["Advanced Protocol Interface", "Application Programming Interface", "All-Purpose Integration", "Automated Program Interaction"],
+        answer: "Application Programming Interface"
+    },
+    {
+        question: "Which HTTP method is primarily used for retrieving data from a server in web development?",
+        options: ["POST", "PUT", "GET", "DELETE"],
+        answer: "GET"
+    },
+    {
+        question: "In responsive web design, what technology is used to adjust the layout and elements based on the user's device screen size?",
+        options: ["HTML", "CSS", "JavaScript", "Media Queries"],
+        answer: "Media Queries"
+    }
+];
+
+export default triviaData;
