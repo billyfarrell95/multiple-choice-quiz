06880b99cb0b165baa8aacfed9bac287d3821509
Billy Farrell <billyfarrell95@gmail.com>
2023-07-29 15:36:47 -0500

    Added event listener to options buttons
    Event listener runs the function that checks the answer

--- js/app.js
+++ js/app.js
@@ -18,10 +18,32 @@ function renderUI() {
         newQuestionWrapper.classList.add("question-wrapper");
         triviaWrapper.append(newQuestionWrapper);
         newQuestionWrapper.append(newH2);
-        for (let j = 0; i < triviaData[i].options.length; j++) {
+        for (let j = 0; j < triviaData[i].options.length; j++) {
             let newBtn = document.createElement("button");
-            newBtn.innerText = triviaData[i].options[i];
+            newBtn.innerText = triviaData[i].options[j];
+            newBtn.setAttribute("data-question-id", triviaData[i].id);
             newQuestionWrapper.append(newBtn);
+
+            newBtn.addEventListener("click", checkAnswerSelection)
         }
     };
 };
+
+function checkAnswerSelection(e) {
+    const selectedBtn = e.target;
+    const selectedAnswer = selectedBtn.innerText;
+    const questionId = selectedBtn.getAttribute("data-question-id");
+
+    let currentQuestion;
+    for (let i = 0; i < triviaData.length; i++) {
+        if (triviaData[i].id.toString() === questionId) {
+            currentQuestion = triviaData[i];
+        }
+    }
+
+    if (selectedAnswer === currentQuestion.answer) {
+        console.log("That is correct");
+    } else {
+        console.log("That is incorrect");
+    }
+}
